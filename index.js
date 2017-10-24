1d7d6d84112d24fa6e552efe15e4ce7dc6941c8c
vbachev <vasil@vasilbachev.com>
2017-10-25 00:15:04 +0300

    Pull out the setting of a spreadsheet id into a separate method

--- index.js
+++ index.js
@@ -1,97 +1,101 @@
 module.exports = GSAPI
 
 // EXAMPLE USAGE:
-// var gsapi = new GSAPI({
-// 	clientId: 'foo',
-// 	spreadsheetId: 'bar'
+// var gsapi = new GSAPI('foo', () => {
+// 	gsapi.setSpreadsheetId('bar')
+// 	gsapi.signIn(() => {
+// 		gsapi.getAll('cars', cars => cars.map(car => console.log(car)))
+// 		gsapi.insert('cars', ['Toyota', 'Prius', 2016, 'any data really'])
+// 	})
 // })
-// gsapi.signIn()
-// gsapi.getAll('cars', cars => cars.map(car => console.log(car)))
-// gsapi.insert('cars', ['Toyota', 'Prius', 2016, 'any data really'])
 
-function GSAPI (config, onInit) {
-	var gapi = window.gapi
+function GSAPI (clientId, onInit) {
+	var _gapi = window.gapi
 
-	if (!gapi)
+	if (!_gapi)
 		throw new Error('GSAPI: Google Sheets API not found! https://apis.google.com/js/api.js must be loaded first.')
-	if (!config)
-		throw new Error('GSAPI: You must provide a configuration object to the constructor. E.g. { clientId: "foo", spreadsheetId: "bar" }')
-	if (!config.clientId)
-		throw new Error('GSAPI: You must provide a clientId as configuration to the constructor. Get one from Google Cloud Console')
-	if (!config.spreadsheetId)
-		throw new Error('GSAPI: You must provide a spreadsheetId as configuration to the constructor. Get one from the adress bar when opening a Google Spreadsheet')
+	if (!clientId)
+		throw new Error('GSAPI: You must provide a clientId as argument to the constructor. Get one from Google Cloud Console')
 
-	var isInitialized = false
-	var isSignedIn = false
+	var _isInitialized = false
+	var _isSignedIn = false
 
-	gapi.load('client:auth2', function () {
+	_gapi.load('client:auth2', function () {
 		var callback = onInit || function () {}
-    gapi.client.init({
+    _gapi.client.init({
 			discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-			clientId: config.clientId,
+			clientId: clientId,
 			scope: 'https://www.googleapis.com/auth/spreadsheets'
 		}).then(function () {
-			isInitialized = true
+			_isInitialized = true
+			_isSignedIn = _gapi.auth2.getAuthInstance().isSignedIn.get()
+			_gapi.auth2.getAuthInstance().isSignedIn.listen(function (value) {
+				_isSignedIn = value
+			})
 			callback()
 		})
   })
 
 	function signIn (callback) {
-		if (!isInitialized) throw new Error('GSAPI: Client API not initialized yet.')
+		if (!_isInitialized) throw new Error('GSAPI: Client API not initialized yet.')
 		callback = callback || function () {}
-		isSignedIn = gapi.auth2.getAuthInstance().isSignedIn.get()
-		if (isSignedIn) {
+		if (_isSignedIn) {
 			callback(true)
 			return
 		}
-		gapi.auth2.getAuthInstance().isSignedIn.listen(function (value) {
-			isSignedIn = value
+		_gapi.auth2.getAuthInstance().isSignedIn.listen(function (value) {
+			// @TODO: this should execute only once (ie .once())
 			callback(value)
 		})
-		gapi.auth2.getAuthInstance().signIn()
+		_gapi.auth2.getAuthInstance().signIn()
 	}
 
 	function signOut () {
-		gapi.auth2.getAuthInstance().signOut()
+		// @TODO: callback
+		_gapi.auth2.getAuthInstance().signOut()
 	}
 
-	function get (resource, id, callback) {
-		checkIfOperational()
-		gapi.client.sheets.spreadsheets.values
+	function setSpreadsheetId (id) {
+		_spreadsheetId = id
+	}
+
+	function get (sheetName, id, callback) {
+		_checkIfOperational()
+		_gapi.client.sheets.spreadsheets.values
 		.get({
-      spreadsheetId: config.spreadsheetId,
-      range: ['' + resource + '!A' + id + ':Z' + id]
+      spreadsheetId: _spreadsheetId,
+      range: ['' + sheetName + '!A' + id + ':Z' + id]
     })
-		.then(parseResponse, handleError)
+		.then(_parseResponse, _handleError)
 		.then(function (data) {
 			return data.values ? data.values[0] : []
 		})
 		.then(callback)
 	}
 
-	function getAll (resource, callback) {
-		checkIfOperational()
-		gapi.client.sheets.spreadsheets.values
+	function getAll (sheetName, callback) {
+		_checkIfOperational()
+		_gapi.client.sheets.spreadsheets.values
 		.batchGet({
-      spreadsheetId: config.spreadsheetId,
-      ranges: ['' + resource + '!A:Z']
+      spreadsheetId: _spreadsheetId,
+      ranges: ['' + sheetName + '!A:Z']
     })
-		.then(parseResponse, handleError)
+		.then(_parseResponse, _handleError)
 		.then(function (data) { return data.valueRanges[0].values })
 		.then(callback)
 	}
 
-	function insert (resource, data, callback) {
-		checkIfOperational()
-		gapi.client.sheets.spreadsheets.values
+	function insert (sheetName, data, callback) {
+		_checkIfOperational()
+		_gapi.client.sheets.spreadsheets.values
 		.append({
-			spreadsheetId: config.spreadsheetId,
-			range: '' + resource + '!A1',
+			spreadsheetId: _spreadsheetId,
+			range: '' + sheetName + '!A1',
 			valueInputOption: 'USER_ENTERED',
 			insertDataOption: 'INSERT_ROWS',
 			values: [data]
 		})
-		.then(parseResponse, handleError)
+		.then(_parseResponse, _handleError)
 		.then(function (data) {
 			var range = data.updatedRange || data.updates.updatedRange
 			return parseInt(range.split('!A')[1].split(':')[0], 10)
@@ -99,50 +103,53 @@ function GSAPI (config, onInit) {
 		.then(callback)
 	}
 
-	function update (resource, id, data, callback) {
-		checkIfOperational()
-		gapi.client.sheets.spreadsheets.values
+	function update (sheetName, id, data, callback) {
+		_checkIfOperational()
+		_gapi.client.sheets.spreadsheets.values
 		.update({
-			spreadsheetId: config.spreadsheetId,
-			range: '' + resource + '!A' + id + ':Z' + id,
+			spreadsheetId: _spreadsheetId,
+			range: '' + sheetName + '!A' + id + ':Z' + id,
 			valueInputOption: 'USER_ENTERED',
 			values: [data]
 		})
-		.then(parseResponse, handleError)
+		.then(_parseResponse, _handleError)
 		.then(function (data) { return true })
 		.then(callback)
 	}
 
-	function remove (resource, id, callback) {
-		checkIfOperational()
-		gapi.client.sheets.spreadsheets.values
+	function remove (sheetName, id, callback) {
+		_checkIfOperational()
+		_gapi.client.sheets.spreadsheets.values
 		.clear({
-			spreadsheetId: config.spreadsheetId,
-			range: '' + resource + '!A' + id + ':Z' + id
+			spreadsheetId: _spreadsheetId,
+			range: '' + sheetName + '!A' + id + ':Z' + id
 		})
-		.then(parseResponse, handleError)
+		.then(_parseResponse, _handleError)
 		.then(function (data) { return true })
 		.then(callback)
 	}
 
-	function checkIfOperational () {
-		if (!isInitialized)
-			throw new Error('GSAPI: Cannot execute operation since the API is not initialized.')
-		if (!isSignedIn)
-			throw new Error('GSAPI: Cannot execute operation since the user is not signed in.')
+	function _checkIfOperational () {
+		if (!_isInitialized)
+			throw new Error('GSAPI: API is not initialized.')
+		if (!_isSignedIn)
+			throw new Error('GSAPI: user is not signed in.')
+		if (!_spreadsheetId)
+			throw new Error('GSAPI: spreadsheetId is not provided. Get one from the adress bar when opening a Google Spreadsheet')
 	}
 
-	function parseResponse (response) {
+	function _parseResponse (response) {
 		return JSON.parse(response.body)
 	}
 
-	function handleError (response) {
+	function _handleError (response) {
 		throw new Error('GSAPI: ' + JSON.parse(response.body).error.message)
 	}
 
 	return {
 		signIn: signIn,
 		signOut: signOut,
+		setSpreadsheetId: setSpreadsheetId,
 		get: get,
 		getAll: getAll,
 		insert: insert,
